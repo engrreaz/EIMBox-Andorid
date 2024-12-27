7f665ef2043e13314786919d263201bcb429c9d4
Reazul Hoque <engrreaz@gmail.com>
2024-12-28 00:42:55 +0600

    android

--- build.php
+++ build.php
@@ -88,7 +88,7 @@ include 'inc.php';
             <div class="col-1 d-flex">
             <i class="bi bi-alt menu-icon text-center"></i>
             </div>
-            <div class="col-11 ps-3 gastr">
+            <div class="col-11 ps-5">
               <h4 class="menu-text">Administrative Setup</h4>
               <div class="menu-sub-title">Class & Sections, Subjects, Teachers, Users etc.</div>
 
