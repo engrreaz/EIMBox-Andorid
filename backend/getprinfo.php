4b1cf0e9d718e637178129b7c2dc17abd9aabf4d
Reazul Hoque <engrreaz@gmail.com>
2025-01-04 02:17:33 +0600

    today-coll,single-pr

--- backend/getprinfo.php
+++ backend/getprinfo.php
@@ -61,7 +61,7 @@ include 'inc.back.php';
 	
 	$lnk = 'https://playstore.eimbox.com/receipt.php?prno='.$prno.'&prdate='.$prdate.'&stname='.$stname.'&cls='.$cls.'&sec='.$sec.'&roll='.$roll.'&total='.$total.'&stid='.$stid.'&collname='.$collname.'&cnt='.$cnt.$loop;
 	// $lnk = 'receipt.php?prno='.$prno.'&prdate='.$prdate.'&stname='.$stname.'&cls='.$cls.'&sec='.$sec.'&roll='.$roll.'&total='.$total.'&stid='.$stid.'&collname='.$collname.'&cnt='.$cnt.$loop;
-	echo $lnk;
+	// echo $lnk;
 	?>
 	
 	<meta http-equiv="refresh" content="0; URL=<?php echo $lnk;?>" />
