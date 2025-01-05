0c3f6030358e481c844d3bf00c91d6b41b1b8458
Reazul Hoque <engrreaz@gmail.com>
2025-01-05 16:54:27 +0600

    today-coll,single-pr

--- stattndregister.php
+++ stattndregister.php
@@ -243,8 +243,8 @@ if ($period >= 2) {
                                 $st_attnd[] = $datam[$my];
                             }
                         }
-
-
+echo '<hr>';
+var_dump($st_att);
 
 
                         if ($status == 0) {
