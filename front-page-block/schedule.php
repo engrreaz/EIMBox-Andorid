d1c3ca2011d47af3abbc8f8be066d1fd968a90a8
Reazul Hoque <engrreaz@gmail.com>
2025-01-07 02:24:09 +0600

    today-coll,single-pr

--- front-page-block/schedule.php
+++ front-page-block/schedule.php
@@ -124,7 +124,7 @@ $dur_sec = strtotime($te) - strtotime($cur);
         <div class="progress" style=" height:3px;">
             <div class="progress-bar progress-danger" role="progressbar" aria-valuenow="70" id="class-progress-bar" aria-valuemin="0"
                 aria-valuemax="100" style="width:70%;">
-                <span class="sr-only" id="class-bar-val">70% Complete</span>
+                <span class="sr-only" id="class-bar-val">100% Complete</span>
             </div>
         </div>
 
