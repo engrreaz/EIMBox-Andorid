ed920df24b3bd1940ab4969bca759b9c4a500401
Reazul Hoque <engrreaz@gmail.com>
2025-01-19 01:44:02 +0600

    today-coll,single-pr

--- sout.php
+++ sout.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 $_SESSION["user"] = '';
+$_SESSION["devicetoken"] = '';
 // unset($_SESSION['token']);
 // unset($_SESSION['userData']);
 // $gClient->revokeToken();
-// session_unset();
-// session_destroy();
-// $_SESSION = NULL;
+session_unset();
+session_destroy();
+$_SESSION = NULL;
 setcookie("user", time() - (3600 * 24 * 365));
 // setcookie("user", "");
 header("Location:index.php");
