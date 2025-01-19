efcb422ae5591f37e3728bce4486437c8e57d3a8
Reazul Hoque <engrreaz@gmail.com>
2025-01-20 04:41:24 +0600

    today-coll,single-pr

--- checkeiin.php
+++ checkeiin.php
@@ -48,6 +48,21 @@ include 'header.php';
 // if (substr($user, 6) > 100000 && strlen($user) >= 10 ) {
 //     echo '<script>alert("XXXPP");</script>';
 // }
+$uuu = [];
+$sql0 = "SELECT * from usersapp where email = '$user' and active=1 and (otp = '$otp' || fixedpin='$otp')";
+// echo $sql0;
+$result0885 = $conn->query($sql0);
+if ($result0885->num_rows > 0) {
+    while ($row0 = $result0885->fetch_assoc()) {
+        $uuu[] = $row0;
+    }
+}
+var_dump($uuu);
+
+
+
+
+
 echo '<div class="mt-5 text-center">';
 
 
@@ -61,7 +76,7 @@ if ($otp == $otp2) {
     $notes = 'Logged in with master key';
     // include 'backend/save-track-book.php';
     $_SESSION["user"] = $user;
-    if ($token_found == 1 && $devicetoken !='') {
+    if ($token_found == 1 && $devicetoken != '') {
         $query33pxy_device_token = "UPDATE usersapp set token='$devicetoken' where email='$usr';";
         $conn->query($query33pxy_device_token);
     }
@@ -93,7 +108,7 @@ if ($otp == $otp2) {
                 $conn->query($query333);
                 $_SESSION["user"] = $user;
                 setcookie("user", time() + (3600 * 24 * 365));
-                if ($token_found == 1 && $devicetoken !='') {
+                if ($token_found == 1 && $devicetoken != '') {
                     $query33pxy_device_token = "UPDATE usersapp set token='$devicetoken' where email='$usr';";
                     $conn->query($query33pxy_device_token);
                 }
