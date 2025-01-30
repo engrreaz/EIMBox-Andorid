3f0414a1f39d3f5a006026b81473d56938925c20
Reazul Hoque <engrreaz@gmail.com>
2025-01-31 01:59:53 +0600

    today-coll,single-pr

--- checkeiin.php
+++ checkeiin.php
@@ -83,7 +83,7 @@ if (count($uuu) == 1) {
         setcookie("user", time() + (3600 * 24 * 365));
         ?>
         <script>
-            window.location.href = 'index.php?email=<?php echo $user; ?>&fullname=<?php echo $uuu[0]['profilename'];?>&truelogin=1<?php echo $gps; ?>';
+            window.location.href = 'index.php?email=<?php echo $user; ?>&fullname=<?php echo $uuu[0]['profilename'];?>&photourl=<?php echo $uuu[0]['photourl'];?>&truelogin=1<?php echo $gps; ?>';
         </script>
         <?php
 
@@ -118,7 +118,7 @@ if (count($uuu) == 1) {
                 ?>
 
                 <script>
-                    window.location.href = 'index.php?email=<?php echo $user; ?>&fullname=<?php echo $uuu[0]['profilename'];?>&truelogin=1<?php echo $gps; ?>';
+                    window.location.href = 'index.php?email=<?php echo $user; ?>&fullname=<?php echo $uuu[0]['profilename'];?>&photourl=<?php echo $uuu[0]['photourl'];?>&truelogin=1<?php echo $gps; ?>';
                 </script>
                 
                 <?php
