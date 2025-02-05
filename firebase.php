3e9ebfa01176d5223c8f3151c59f2d502c992828
Reazul Hoque <engrreaz@gmail.com>
2025-02-06 05:10:41 +0600

    Fix-Class-routine-setup

--- firebase.php
+++ firebase.php
@@ -9,15 +9,16 @@ $FcmApi = new \FcmApi(
     ]
 );
 
-// $firebase_datam = array(
-//     'title' => 'Hello Sir',
-//     'body' => 'Your next class at Nine (Rajanigandha) on ICT after 10 minutes.',
-//     'image' => 'https://eimbox.com/logo/105673.png',
-//     'data1' => '',
-//     'data2' => '',
-//     'data3' => '',
-// );
-$token = 'cMqpsuYWQaOo1W0bnb2OUO:APA91bFuYN0oKVjYItycw3n3I4oQinwWLJyAsSbjsCNih3zmFb0TGRd2rSR35lrf37uvc6MputEmj0xL6X9UJ8a1nbbPBjZk5qPM9R12eIWH6WIQ66dxn8c';
+$firebase_datam = array(
+    'title' => 'Hello Sir',
+    'body' => 'Your next class at Nine (Rajanigandha) on ICT after 10 minutes.',
+    'image' => '',
+    'data1' => '',
+    'data2' => '',
+    'data3' => '',
+);
+
+// $token = 'cMqpsuYWQaOo1W0bnb2OUO:APA91bFuYN0oKVjYItycw3n3I4oQinwWLJyAsSbjsCNih3zmFb0TGRd2rSR35lrf37uvc6MputEmj0xL6X9UJ8a1nbbPBjZk5qPM9R12eIWH6WIQ66dxn8c';
 echo $token . '<br><br>';
 
 if (
