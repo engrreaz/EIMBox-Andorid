a829f3b301fc9ab5d08ed48bbcaf8688afcce240
Reazul Hoque <engrreaz@gmail.com>
2025-02-07 01:28:17 +0600

    Implement-absent-bunk

--- make-call.php
+++ make-call.php
@@ -1,10 +1,18 @@
+<?php
+include 'inc.php';
+?>
+<div class="st-id mt-5 text-center" id="foc"></div>
+
+
 <script>
+
     // 
     // history.back();
 
     $(window).focus(function () {
-        console.log('welcome (back)');
-        const myTimeout = setTimeout(history.back(), 3000);
+        alert('fouced');
+        // console.log('welcome (back)');
+        // const myTimeout = setTimeout(history.back(), 3000);
     });
 
 </script>
