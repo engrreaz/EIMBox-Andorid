ea61fd9ce1f70d2721c7287ac012f44952c37c60
Reazul Hoque <engrreaz@gmail.com>
2025-02-07 01:18:21 +0600

    Implement-absent-bunk

--- make-call.php
+++ make-call.php
@@ -1,4 +1,10 @@
 <script>
-    // const myTimeout = setTimeout(history.back(), 3000);
+    // 
     // history.back();
+
+    $(window).focus(function () {
+        console.log('welcome (back)');
+        const myTimeout = setTimeout(history.back(), 3000);
+    });
+
 </script>
