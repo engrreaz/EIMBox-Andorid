7818fb10fad730856e744ce09f6f8cd43ead9b99
Reazul Hoque <engrreaz@gmail.com>
2025-02-07 01:43:25 +0600

    Implement-absent-bunk

--- make-call.php
+++ make-call.php
@@ -1,16 +1,21 @@
 <?php
 include 'inc.php';
 ?>
-<div class="st-id mt-5 text-center" id="foc">.....</div>
+<div class="st-id mt-5 text-center" id="foc"></div>
 
 
 <script>
-   alert('fouced');
+   var elem = document.getElementById("foc");
+   if(elem.innerHTML==''){
+    elem.innerHTML="Calling...";
+   } else {
+    history.back();
+   }
     // 
-    // history.back();
-    $( document ).ready(){
+    // 
+    // $( document ).ready(){
         
-    }
+    // }
    
 
 </script>
