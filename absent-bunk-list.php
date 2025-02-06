9a3a4fe959fd3634b88c2bf21769f3de05be8a40
Reazul Hoque <engrreaz@gmail.com>
2025-02-07 01:01:05 +0600

    Implement-absent-bunk

--- absent-bunk-list.php
+++ absent-bunk-list.php
@@ -449,10 +449,11 @@ $count_class = count($cteacher_data);
   function send_absent_notice(stid, way, mobile) {
     event.stopPropagation();
     // mobile = '01919629672';
-
+    var lnk = "make-call.php?mobilenumber=" + mobile;
+    alert(lnk);
     // if (way == 0) {
     // alert(mobile);
-    window.location.href = "make-call.php?mobilenumber=" + mobile;
+    window.location.href = lnk;
     // }
     // $("#blocksel" + id).html("");
 
