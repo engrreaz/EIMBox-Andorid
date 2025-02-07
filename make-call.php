d0783030ac10ff84750fb561a34b7e4d2575e620
Reazul Hoque <engrreaz@gmail.com>
2025-02-07 23:03:31 +0600

    Fixing-phone-call

--- make-call.php
+++ make-call.php
@@ -5,31 +5,23 @@ include 'inc.php';
 
 
 <script>
-var isTabActive;
- var tx = document.getElementById("foc");
-window.onfocus = function () { 
-//   isTabActive = true; 
-//   alert('A');
- if(tx.innerHTML == ''){
-    tx.innerHTML = "Calling...";
- } else {
-    history.back();
- }
+    var isTabActive;
+    var tx = document.getElementById("foc");
+    window.onfocus = function () {
+        //   isTabActive = true; 
+        //   alert('A');
 
-}; 
+        history.back();
+    };
 
-window.onblur = function () { 
-    if(tx.innerHTML == ''){
-    tx.innerHTML = "Calling...";
- } else {
-    history.back();
- }
-}
+    window.onblur = function () {
+        tx.innerHTML = "Calling...";
+    }
 
-// test
-// setInterval(function () { 
-//   alert(window.isTabActive ? 'active' : 'inactive'); 
-// }, 1000);
+    // test
+    // setInterval(function () { 
+    //   alert(window.isTabActive ? 'active' : 'inactive'); 
+    // }, 1000);
 
 
 
