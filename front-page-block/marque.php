05f37730a51b5adb74d27308958962b1ef7809b5
Reazul Hoque <engrreaz@gmail.com>
2025-03-16 15:44:04 +0600

    Fixing-phone-call

--- front-page-block/marque.php
+++ front-page-block/marque.php
@@ -4,7 +4,8 @@ $speed = "5";
 <marquee behavior="scroll" direction="left" scrollamount="<?php echo $speed; ?>">
     <?php
     foreach ($notices as $notice) {
-        echo htmlspecialchars($notice['descrip']);
+        // echo htmlspecialchars($notice['descrip']);
+        echo $notice['descrip'];
         echo ' -- ';
     }
 
