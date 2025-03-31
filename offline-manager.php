36e5706f2a531887b5f573b011b7c712f80b95c6
Reazul Hoque <engrreaz@gmail.com>
2025-03-31 17:48:13 +0600

    Fixing-Session-Error

--- offline-manager.php
+++ offline-manager.php
@@ -476,7 +476,7 @@ if (strpos($profile_entry, $userlevel) != null) {
     var rollno = document.getElementById('rollno' + i).innerHTML;
     var stid = document.getElementById('stid' + i).innerHTML;
     var stname = document.getElementById('stname' + i).innerHTML;
-    var str = '{"' + i + '": { "rollno": ' + rollno + ', "stid": ' + stid + ', "stname": ' + stname + ', "yn": 0 },}, ';
+    var str = '{' ': { "rollno": ' + rollno + ', "stid": ' + stid + ', "stname": ' + stname + ', "yn": 0 },}, ';
     // var strstr = JSON.stringify(str);
     full_str += str;
   }
