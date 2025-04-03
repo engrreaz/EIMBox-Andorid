6ae6b7a1c97e371c87034885fcf2b1ef106c4a45
Reazul Hoque <engrreaz@gmail.com>
2025-04-04 02:26:13 +0600

    Fixing-Session-Error

--- offline-manager.php
+++ offline-manager.php
@@ -562,8 +562,8 @@ if (strpos($profile_entry, $userlevel) != null) {
 
     alert("Action" + jsonData);
 
-    var stc = jsonPata[myself][stcount];
-    alert(stc);
+    var stc = stcount;// jsonPata[myself][stcount];
+    alert(stcount);
     var yy = '';
     for (var d = 0; d < stc; d++) {
       var sing = jsonPata[myself][cls][sec][d]['yn'].replaceAll('"', '');;
