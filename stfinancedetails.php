ad19ec7ca35df769bad7d2e488345d9c19656dd9
Reazul Hoque <engrreaz@gmail.com>
2025-11-09 02:18:59 +0600

    upload

--- stfinancedetails.php
+++ stfinancedetails.php
@@ -76,6 +76,7 @@ if ($result5r->num_rows > 0) {
 
 
     $sql00x = "SELECT prno FROM stpr where  sccode='$sccode' and stid='$stid' and sessionyear= '$ssyy' order by prno DESC LIMIT 1";
+    echo  $sql00x;
     $result00x = $conn->query($sql00x);
     if ($result00x->num_rows > 0) {
         while ($row00x = $result00x->fetch_assoc()) {
