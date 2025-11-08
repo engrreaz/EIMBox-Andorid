841db745404a023c10b549598b88d3d7d2e43d25
Reazul Hoque <engrreaz@gmail.com>
2025-11-09 02:26:16 +0600

    upload

--- stfinancedetails.php
+++ stfinancedetails.php
@@ -76,7 +76,7 @@ if ($result5r->num_rows > 0) {
 
 
     $sql00x = "SELECT prno FROM stpr where  sccode='$sccode' and stid='$stid' and sessionyear LIKE '%$sy%' order by prno DESC LIMIT 1";
-    echo  $sql00x;
+    // echo  $sql00x;
     $result00x = $conn->query($sql00x);
     if ($result00x->num_rows > 0) {
         while ($row00x = $result00x->fetch_assoc()) {
@@ -85,7 +85,8 @@ if ($result5r->num_rows > 0) {
     } else {
         $lastpr = 0;
     }
-    echo '.................' . $lastpr;
+
+    
 
     if ($lastpr > 0) {
         $prno = $lastpr + 1;
@@ -95,7 +96,7 @@ if ($result5r->num_rows > 0) {
         $prno = ($SY % 100) * 1000000 + ($stid % 10000) * 100 + 1;
     }
 
-echo $prno;
+// echo $prno;
 
     if ($userlevel == 'Administrator' or $userlevel == 'Super Administrator') {
         $dtds = '';
